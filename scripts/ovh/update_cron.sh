26ccd0f0741ecae3405c104c77bb67c4ac13d014
Maël LE GAL <mael.le-gal@corp.ovh.com>
2019-07-26 13:54:35 +0200

    Add healthcheck

--- scripts/ovh/update_cron.sh
+++ scripts/ovh/update_cron.sh
@@ -7,3 +7,11 @@ kubectl create -n ovh configmap script-clean-harbor-registry --from-file=scripts
 echo "Updating the k8s cronjob"
 
 kubectl apply -n ovh -f config/ovh/cron_clean_harbor_registry.yaml
+
+echo "Updating the python script for healthcheck configmap"
+
+kubectl create -n metrics configmap script-healthcheck --from-file=scripts/ovh/healthcheck.py --dry-run -o yaml | kubectl apply -f -
+
+echo "Updating the k8s cronjob for healthcheck"
+
+kubectl apply -n metrics -f config/ovh/cron_healthcheck.yaml
